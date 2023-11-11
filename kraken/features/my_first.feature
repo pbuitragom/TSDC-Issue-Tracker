c274b9e86104ce2de6b8b11873527bdb9afdec18
Pedro Maria Buitrago <pedbui.ext@eltiempo.com.co>
2023-11-11 14:00:38 -0500

    Homologando properties, steps y feature

--- kraken/features/my_first.feature
+++ kraken/features/my_first.feature
@@ -1,11 +1,27 @@
-Feature: My feature
+Feature: Iniciar una conversación
 
-@user1 @mobile
-Scenario: My scenario 1
-  Given I wait
-  When I send a signal to user 2 containing "hi"
+@user1 @web
+Scenario: Como primer usuario inicio sesion y mandó un mensaje al usuario 2
+  Given I navigate to page "https://www.messenger.com/login"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME1>"
+  And I wait for 2 seconds  
+  And I enter password "<PASSWORD1>"
+  And I wait for 2 seconds
+  And I click next
+  And I wait for 7 seconds
+  Then I send a signal to user 2 containing "login1 complete"
+  And I wait for a signal containing "login2 complete" for 15 seconds
 
 @user2 @web
-Scenario: My scenario 2
-  Given I wait for a signal containing "hi"
-  When I wait
+Scenario: Como usuario 2 inicio sesion y mandó un mensaje al usuario 1
+  Given I navigate to page "https://www.messenger.com/login"
+  And I wait for 5 seconds
+  When I enter email "<USERNAME2>"
+  And I wait for 2 seconds
+  And I enter password "<PASSWORD2>"
+  And I wait for 7 seconds
+  And I wait for a signal containing "login1 complete" for 15 seconds
+  And I click next
+  And I wait for 7 seconds
+  And I send a signal to user 1 containing "login2 complete"
--- kraken/features/web/step_definitions/step.js
+++ kraken/features/web/step_definitions/step.js
@@ -1,1 +1,33 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+When('I enter email {kraken-string}', async function (email) {
+    let element = await this.driver.$('#email');
+    return await element.setValue(email);
+});
+
+When('I enter password {kraken-string}', async function (password) {
+    let element = await this.driver.$('#pass');
+    return await element.setValue(password);
+});
+
+When('I click next', async function() {
+    let element = await this.driver.$('#loginbutton');
+    return await element.click();
+})
+
+Then('I click on the first conversation', async function () {
+    //let element = await this.driver.$('x1av1boa > div:nth-child(1) > div:nth-child(1)');
+    let element = await this.driver.$('#\:r2s\: > div > div > div > div > div:nth-child(2) > div > div.html-div.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd > div > div > a > div > div > div > div:nth-child(1) > div > div > div.x9f619.x1n2onr6.x1ja2u2z.xsdox4t.x1useyqa > div > div > div > div > div > div');
+    return await element.click();
+});
+
+Then('I click on the redact message inputbox', async function () {
+    //let element = await this.driver.$('p.xat24cr');
+    let element = await this.driver.$('#mount_0_0_6K > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div > div.x78zum5.xdt5ytf.x1t2pt76.x1n2onr6.x1ja2u2z.x10cihs4 > div.x9f619.x2lah0s.x1nhvcw1.x1qjc9v5.xozqiw3.x1q0g3np.x78zum5.x1iyjqo2.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.x1n2onr6.x1ja2u2z.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.x78zum5.x1t2pt76 > div > div > div > div > div > div > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div:nth-child(2) > div > div > div.x78zum5.x1iyjqo2.x6q2ic0 > div.xmjcpbm.x107yiy2.xv8uw2v.x1tfwpuw.x2g32xy.x9f619.x1iyjqo2.xeuugli > div > div > div > p');
+    return await element.click();
+  });
+
+Then('I send the message', async function () {
+    let element = await this.driver.$('.x4k7w5x x1h91t0o x1h9r5lt xv2umb2 x1beo9mf xaigb6o x12ejxvf x3igimt xarpa2k xedcshv x1lytzrv x1t2pt76 x7ja8zs x1qrby5j x3nfvp2');
+    return await element.click();
+});
