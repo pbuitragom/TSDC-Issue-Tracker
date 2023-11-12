9565d20e7fa37a7c1eeacd2e7a71fb0bd52c0080
jdrada <j.ortizd@uniandes.edu.co>
2023-11-12 01:41:05 -0500

    Delete Member e2e Test - Members functionality

---
+++ cypress/cypress/e2e/Members/delete_member_spec.cy.js
@@ -0,0 +1,13 @@
+describe("Post Creation and Publishing", () => {
+  beforeEach(() => {
+    cy.login(Cypress.env("email"), Cypress.env("password"));
+    cy.visit(`${Cypress.env("login_url")}/#/members`);
+  });
+
+  it("allows a user to update a internal Tag", () => {
+    cy.get('tr[data-test-list="members-list-item"]').first().click();
+    cy.get('button[data-test-button="member-actions"]').click();
+    cy.get('button[data-test-button="delete-member"]').click();
+    cy.contains("Delete member account").should("be.visible");
+  });
+});
